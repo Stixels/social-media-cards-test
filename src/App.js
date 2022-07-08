464f835fe9f7414d1d16eb86521fba423cd97b6a
Matthew Mercado <mmercado@box.com>
2022-07-08 11:40:42 -0400

    Debugging

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,23 +7,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <head>
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@flickr" />
-          <meta
-            name="twitter:title"
-            content="Small Island Developing States Photo Submission"
-          />
-          <meta
-            name="twitter:description"
-            content="View the album on Flickr."
-          />
-          <meta
-            name="twitter:image"
-            content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg"
-          />
-        </head>
         <header className="App-header">
+          <Helmet>
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="@flickr" />
+            <meta
+              name="twitter:title"
+              content="Small Island Developing States Photo Submission"
+            />
+            <meta
+              name="twitter:description"
+              content="View the album on Flickr."
+            />
+            <meta
+              name="twitter:image"
+              content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg"
+            />
+          </Helmet>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
