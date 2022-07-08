f7eb8c81ca901113101af9b559ba14e9641ef403
Matthew Mercado <mmercado@box.com>
2022-07-08 11:28:21 -0400

    Redo

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import logo from "./logo.svg";
 import "./App.css";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Helmet className="App-header">
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@nytimes" />
-          <meta name="twitter:creator" content="@SarahMaslinNir" />
-          <meta name="twitter:title" content="Cards test" />
-          <meta name="twitter:description" content="Quick media card test" />
+        <Helmet>
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@flickr" />
+          <meta
+            name="twitter:title"
+            content="Small Island Developing States Photo Submission"
+          />
+          <meta
+            name="twitter:description"
+            content="View the album on Flickr."
+          />
           <meta
             name="twitter:image"
-            content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"
+            content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg"
           />
-
+        </Helmet>
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-        </Helmet>
+        </header>
       </div>
     );
   }
