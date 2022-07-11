b18961cb6b16d08ac0f04142c25bc015e8ca8b23
Matthew Mercado <mmercado@box.com>
2022-07-11 14:43:56 -0400

    Remove helmet tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Helmet>
+        {/* <Helmet>
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@Box" />
           <meta name="twitter:title" content="Social Media Card test" />
@@ -16,7 +16,7 @@ class App extends Component {
             name="twitter:image"
             content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg"
           />
-        </Helmet>
+        </Helmet> */}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
